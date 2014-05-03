2c622970540e2e1fd61da061f56be9e2c4338601
Adrian L Lange <adrianlund@gmail.com>
2014-05-03 10:16:46 +0200

    Start off easy with an empty file

---
+++ App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lackey
+{
+	public class App : Form
+	{
+		[STAThread]
+		public static void Main()
+		{
+			Application.Run(new App());
+		}
+
+		public App()
+		{
+
+		}
+	}
+}
