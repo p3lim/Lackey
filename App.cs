001a43e848d3e217871d0e5e8a6f3aed32a79b23
Adrian L Lange <adrianlund@gmail.com>
2014-05-03 10:45:12 +0200

    Embed the libraries on assembly, and allow for that

--- App.cs
+++ App.cs
@@ -2,6 +2,7 @@
 using MouseKeyboardActivityMonitor;
 using MouseKeyboardActivityMonitor.WinApi;
 using System;
+using System.Reflection;
 using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
@@ -28,6 +29,19 @@ namespace Lackey
 
 		public App()
 		{
+			AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+			{
+				string resourceName = new AssemblyName(args.Name).Name + ".dll";
+				string resource = Array.Find(this.GetType().Assembly.GetManifestResourceNames(), element => element.EndsWith(resourceName));
+
+				using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+				{
+					Byte[] assemblyData = new Byte[stream.Length];
+					stream.Read(assemblyData, 0, assemblyData.Length);
+					return Assembly.Load(assemblyData);
+				}
+			};
+
 			InitializeComponent();
 		}
 
