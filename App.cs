2367df5f8f9920227d36001c2990dab02b544605
Adrian L Lange <adrianlund@gmail.com>
2014-05-03 10:19:07 +0200

    Add a tray icon to control it

--- App.cs
+++ App.cs
@@ -11,9 +11,38 @@ namespace Lackey
 			Application.Run(new App());
 		}
 
+		private NotifyIcon trayIcon;
+		private ContextMenu trayMenu;
+
 		public App()
 		{
+			InitializeComponent();
+		}
 
+		private void InitializeComponent()
+		{
+			trayMenu = new ContextMenu();
+			trayMenu.MenuItems.Add("Exit", OnExit);
+
+			trayIcon = new NotifyIcon();
+			trayIcon.Text = "Lackey";
+			trayIcon.ContextMenu = trayMenu;
+			trayIcon.Visible = true;
+		}
+
+		public void OnExit(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				trayIcon.Dispose();
+			}
+
+			base.Dispose(disposing);
 		}
 	}
 }
